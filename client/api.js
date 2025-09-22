9db4f97e381569ad56fb8071c1b47bc4e7df97e9
Ashmalik28 <ashyammalik052@gmail.com>
2025-09-23 01:05:58 +0530

    connected signup route with signup page

---
+++ client/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL : "http://localhost:3000"
+});
+
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+});
+
+export const signup = async (data) => {
+    const res = await API.post("/signup" , data);
+    return res.data;
+}
+
+export const signin = async (data) => {
+    const res = await API.post("/signin" , data);
+    localStorage.setItem("token" , res.data.token);
+    return res.data;
+}
+
+export default API;
